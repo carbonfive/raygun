b71c807e4f9486ce34d10bee38cb9089e6f01a7b
Christian Nelson <christian@carbonfive.com>
2012-09-28 09:38:39 -0700

    Clearer description of how sample_data.rb is to be used.

--- templates/_db/sample_data.rb
+++ templates/_db/sample_data.rb
@@ -1,13 +1,13 @@
 # Populate the database with a small set of realistic sample data so that as a developer/designer, you can use the
-# application without having to create a bunch of stuff (or pull down production data).
+# application without having to create a bunch of stuff or pull down production data.
 #
 # After running db:sample_data, a developer/designer should be able to fire up the app, sign in, browse data and see
 # examples of practically anything (interesting) that can happen in the system.
 #
-# It's a good idea to build this up along with the features; when you build a feature, make sure you can demo it
-# after running db:populate.
+# It's a good idea to build this up along with the features; when you build a feature, make sure you can easily demo it
+# after running db:sample_data.
 #
-# Nothing in the file that is absolutely required to run the application (i.e. reference data) should be included here.
+# Data that is required by the application across all environments (i.e. reference data) should _not_ be included here.
 # That belongs in seeds.rb instead.
 
 User.create! do |u|
@@ -15,4 +15,3 @@ User.create! do |u|
   u.name     = 'John Smith'
   u.password = 'password'
 end.activate!
-
