dc7cb390e38600359e15fdba947300f5be07cc03
Jason Wadsworth <jdwadsworth@gmail.com>
2012-10-04 09:06:58 -0700

    Add filters and groups to Simplecov report

--- templates/_spec/support/simplecov.rb
+++ templates/_spec/support/simplecov.rb
@@ -12,5 +12,14 @@ if ENV['COVERAGE']
   end
   SimpleCov.formatter = SimpleCov::Formatter::QualityFormatter
 
-  SimpleCov.start
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_filter '/config/'
+    add_filter '/vendor/'
+    add_group 'Models', 'app/models'
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Views', 'app/views'
+    add_group 'Mailers', 'app/mailers'
+  end
 end
