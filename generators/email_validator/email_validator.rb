bdae1cc5c6bb5d450262fb698fe866ed54f5cfc2
Christian Nelson <christian@carbonfive.com>
2012-10-31 12:11:18 -0700

    Use a gem for email validation, it's silly to reinvent this.

--- generators/email_validator/email_validator.rb
+++
@@ -1,9 +0,0 @@
-class EmailValidator < ActiveModel::EachValidator
-
-  def validate_each(record, attribute, value)
-    unless value =~ /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-
-end
