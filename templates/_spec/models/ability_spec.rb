75b85f579a86607e31679d8f61c90c42867e032f
Christian Nelson <christian@carbonfive.com>
2012-10-31 15:20:50 -0700

    Flesh out the ability specs and refactor so they don't have to interact with the database.

--- templates/_spec/models/ability_spec.rb
+++ templates/_spec/models/ability_spec.rb
@@ -2,30 +2,35 @@ require 'spec_helper'
 require 'cancan/matchers'
 
 describe "User" do
-  let(:user) { create(:user) }
-  let(:ability) { Ability.new(user) }
-
-  subject { ability }
-
   context "when working with User" do
     context "as a non-admin" do
+      let(:user) { build(:user) { |u| u.id = 1 } }
+      subject { Ability.new(user) }
+
       context "operating on themselves" do
         it { should be_able_to(:manage, user) }
       end
 
       context "operating on someone else" do
-        let(:other) { create(:user) }
+        let(:other) { build(:user) { |u| u.id = 2 } }
 
         it { should_not be_able_to(:manage, other) }
       end
     end
 
     context "as an admin" do
-      let(:user) { create(:admin) }
-      let(:ability) { Ability.new(user) }
-      let(:other) { create(:user) }
+      let(:user) { build(:admin) { |u| u.id = 1 } }
+      subject { Ability.new(user) }
 
-      it { should be_able_to(:manage, other) }
+      context "operating on themselves" do
+        it { should be_able_to(:manage, user) }
+      end
+
+      context "operating on someone else" do
+        let(:other) { build(:user) { |u| u.id = 2 } }
+
+        it { should be_able_to(:manage, other) }
+      end
     end
   end
 end
