a3d427828a45c057e423f50e1d58bcf45b2bbb6a
Christian Nelson <christian@carbonfive.com>
2012-12-20 10:28:20 -0800

    Use the new expect() syntax. In fact, disallow the old syntax.

--- app_prototype/spec/views/users/new.html.slim_spec.rb
+++ app_prototype/spec/views/users/new.html.slim_spec.rb
@@ -11,7 +11,6 @@ describe "users/new" do
   it "renders new user form" do
     render
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", action: users_path, method: "post" do
       assert_select "input#user_email", name: "user[email]"
       assert_select "input#user_name", name: "user[name]"
--- app_prototype/spec/views/users/show.html.slim_spec.rb
+++ app_prototype/spec/views/users/show.html.slim_spec.rb
@@ -10,8 +10,7 @@ describe "users/show" do
 
   it "renders attributes in <p>" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Email/)
-    rendered.should match(/Name/)
+    expect(rendered).to match /Email/
+    expect(rendered).to match /Name/
   end
 end
