858080f8651f3c4156b7e80af6fde0d094b0cfd0
Christian Nelson <christian@carbonfive.com>
2012-12-21 14:38:41 -0800

    Consistent hostnames based on app name.

--- app_prototype/config/environments/production.rb
+++ app_prototype/config/environments/production.rb
@@ -65,5 +65,5 @@ AppPrototype::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { host: 'app_prototype.com' }
+  config.action_mailer.default_url_options = { host: 'app_prototype.herokuapp.com' }
 end
