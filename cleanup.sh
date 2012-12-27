6f1ef6a2567963ba0b44fea68312a781022e7b97
Christian Nelson <christian@carbonfive.com>
2012-12-26 21:37:13 -0800

    Remove all instances of .DS_Store.

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# All git ignored files: git ls-files --others -i --exclude-standard
+
 git clean -df
 git checkout app_prototype/config/routes.rb app_prototype/db/schema.rb
 
+find . -name ".DS_Store" -exec rm -f {} \;
+
+rm -rfv app_prototype/.DS_Store
 rm -rfv app_prototype/log/*
 rm -rfv app_prototype/tmp/*
 rm -rfv app_prototype/coverage
