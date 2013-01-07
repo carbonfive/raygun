4952ab8fbe117bf9c5d23b691dcef2ab60cc9532
Christian Nelson <christian@carbonfive.com>
2013-01-07 13:49:07 -0800

    Explain slow queries in acceptance too.

--- app_prototype/config/environments/acceptance.rb
+++ app_prototype/config/environments/acceptance.rb
@@ -63,7 +63,7 @@ AppPrototype::Application.configure do
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { host: 'app-prototype-acceptance.herokuapp.com' }
 end
