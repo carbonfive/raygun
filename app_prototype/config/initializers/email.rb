cf2a08b353ec03946bda8ab5a943e223e1a94659
Christian Nelson <christian@carbonfive.com>
2013-01-07 13:21:46 -0800

    DRY up the mailcatcher port.

--- app_prototype/config/initializers/email.rb
+++ app_prototype/config/initializers/email.rb
@@ -1,11 +1,13 @@
 # http://www.mikeperham.com/2012/12/09/12-gems-of-christmas-4-mailcatcher-and-mail_view/
 
+mailcatcher_port = 1025
+
 begin
-  sock = TCPSocket.new('localhost', 1025)
+  sock = TCPSocket.new('localhost', mailcatcher_port)
   sock.close
   catcher = true
 rescue
   catcher = false
 end
 
-ActionMailer::Base.smtp_settings = (Rails.env.development? && catcher) ? { host: 'localhost', port: '1025', } : {}
+ActionMailer::Base.smtp_settings = (Rails.env.development? && catcher) ? { host: 'localhost', port: mailcatcher_port, } : {}
