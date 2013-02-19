47a16a940e15a5b10fc75775a43913c26f4cbe2b
Christian Nelson <christian@carbonfive.com>
2013-02-18 19:57:23 -0800

    :index, :show, :new, and :edit all use factory_girl's build_stubbed and ruby 1.9 hash syntax.

--- app_prototype/lib/templates/rspec/scaffold/edit_spec.rb
+++ app_prototype/lib/templates/rspec/scaffold/edit_spec.rb
@@ -1,16 +1,18 @@
 require 'spec_helper'
 
 <% output_attributes = attributes.reject{|attribute| [:datetime, :timestamp, :time, :date].index(attribute.type) } -%>
-describe "<%= ns_table_name %>/show" do
+describe "<%= ns_table_name %>/edit" do
   before(:each) do
     @<%= ns_file_name %> = assign(:<%= ns_file_name %>, build_stubbed(:<%= class_name.underscore %>))
   end
 
-  it "renders attributes" do
+  it "renders the edit <%= ns_file_name %> form" do
     render
 
+    assert_select "form", action: <%= index_helper %>_path(@<%= ns_file_name %>), method: "post" do
 <% for attribute in output_attributes -%>
-    expect(rendered).to match(/<%= eval(value_for(attribute)) %>/)
+      assert_select "<%= attribute.input_type -%>#<%= ns_file_name %>_<%= attribute.name %>", name: "<%= ns_file_name %>[<%= attribute.name %>]"
 <% end -%>
+    end
   end
 end
