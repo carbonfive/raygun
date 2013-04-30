5caa247bc761ff6f01980870144e6aecded9f75c
Christian Nelson <christian@carbonfive.com>
2013-04-29 21:22:59 -0700

    Disable unwanted generators.

--- rails_40/config/application.rb
+++ rails_40/config/application.rb
@@ -5,7 +5,6 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -16,6 +15,15 @@ module AppPrototype
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Disable unwanted generators.
+    config.generators do |generate|
+      generate.stylesheets   false
+      #generate.helper        false
+      generate.routing_specs false
+      #generate.view_specs    false
+      generate.request_specs false
+    end
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
