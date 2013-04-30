3947a71ec1d9ba34bb009a7f84695250b9e3fcfa
Christian Nelson <christian@carbonfive.com>
2013-04-29 21:37:58 -0700

    Default hosts for action mailer.

--- rails_40/config/environments/production.rb
+++ rails_40/config/environments/production.rb
@@ -77,4 +77,6 @@ AppPrototype::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: 'app-prototype.herokuapp.com' }
 end
